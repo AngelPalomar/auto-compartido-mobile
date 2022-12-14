9204dc6681a631b4725639586a63c71832c83eb3
AngelPalomar <angelpg.123@hotmail.com>
2022-12-13 19:35:54 -0600

    Cambiados dependencias de galería, indentado de App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from "react";
 import { LogBox } from 'react-native'
 import {
-  Text,
-  HStack,
-  Switch,
-  useColorMode,
-  NativeBaseProvider,
-  extendTheme
+	Text,
+	HStack,
+	Switch,
+	useColorMode,
+	NativeBaseProvider,
+	extendTheme
 } from "native-base";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -23,100 +23,100 @@ import CrearRuta from "./screens/private/conductor/CrearRuta";
 
 // Define the config
 const config = {
-  useSystemColorMode: false,
-  initialColorMode: "light",
+	useSystemColorMode: false,
+	initialColorMode: "light",
 };
 
 // extend the theme
 export const theme = extendTheme({ config });
 type MyThemeType = typeof theme;
 declare module "native-base" {
-  interface ICustomTheme extends MyThemeType { }
+	interface ICustomTheme extends MyThemeType { }
 }
 
 //create navigation stack routing
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
-  useEffect(() => {
-    LogBox.ignoreLogs([
-      'Setting a timer',
-      'AsyncStorage has been extracted from react-native core'
-    ]);
-  }, []);
+	useEffect(() => {
+		LogBox.ignoreLogs([
+			'Setting a timer',
+			'AsyncStorage has been extracted from react-native core'
+		]);
+	}, []);
 
-  return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="IniciarSesion" component={IniciarSesion} options={{
-            headerShown: false
-          }} />
-          <Stack.Screen name="RegistrarsePasajero" component={RegistrarsePasajero} options={{
-            title: 'Crear una cuenta de pasajero',
-            headerStyle: {
-              backgroundColor: theme.colors.darkBlue[800]
-            },
-            headerTintColor: '#FFFFFF'
-          }} />
-          <Stack.Screen name="RegistrarseConductor" component={RegistrarseConductor} options={{
-            title: 'Crear una cuenta como conductor',
-            headerStyle: {
-              backgroundColor: theme.colors.darkBlue[800]
-            },
-            headerTintColor: '#FFFFFF'
-          }} />
-          <Stack.Screen name="RegistrarVehiculo" component={RegistrarVehiculo} options={{
-            title: 'Datos de tu vehiculo',
-            headerStyle: {
-              backgroundColor: theme.colors.darkBlue[800]
-            },
-            headerTintColor: '#FFFFFF'
-          }} />
-          <Stack.Screen name="RegistrarLicencia" component={RegistrarLicencia} options={{
-            title: 'Datos de tu licencia',
-            headerStyle: {
-              backgroundColor: theme.colors.darkBlue[800]
-            },
-            headerTintColor: '#FFFFFF'
-          }} />
-          <Stack.Screen name="Inicio" component={Inicio} options={{
-            headerShown: false
-          }} />
-          <Stack.Screen name="VerConductor" component={VerConductor} options={{
-            title: '',
-            headerStyle: {
-              backgroundColor: theme.colors.darkBlue[800]
-            },
-            headerTintColor: '#FFFFFF'
-          }} />
-          <Stack.Screen name="CrearRuta" component={CrearRuta} options={{
-            title: 'Crear una nueva ruta',
-            headerStyle: {
-              backgroundColor: theme.colors.darkBlue[800]
-            },
-            headerTintColor: '#FFFFFF'
-          }} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
-  );
+	return (
+		<NativeBaseProvider>
+			<NavigationContainer>
+				<Stack.Navigator>
+					<Stack.Screen name="IniciarSesion" component={IniciarSesion} options={{
+						headerShown: false
+					}} />
+					<Stack.Screen name="RegistrarsePasajero" component={RegistrarsePasajero} options={{
+						title: 'Crear una cuenta de pasajero',
+						headerStyle: {
+							backgroundColor: theme.colors.darkBlue[800]
+						},
+						headerTintColor: '#FFFFFF'
+					}} />
+					<Stack.Screen name="RegistrarseConductor" component={RegistrarseConductor} options={{
+						title: 'Crear una cuenta como conductor',
+						headerStyle: {
+							backgroundColor: theme.colors.darkBlue[800]
+						},
+						headerTintColor: '#FFFFFF'
+					}} />
+					<Stack.Screen name="RegistrarVehiculo" component={RegistrarVehiculo} options={{
+						title: 'Datos de tu vehiculo',
+						headerStyle: {
+							backgroundColor: theme.colors.darkBlue[800]
+						},
+						headerTintColor: '#FFFFFF'
+					}} />
+					<Stack.Screen name="RegistrarLicencia" component={RegistrarLicencia} options={{
+						title: 'Datos de tu licencia',
+						headerStyle: {
+							backgroundColor: theme.colors.darkBlue[800]
+						},
+						headerTintColor: '#FFFFFF'
+					}} />
+					<Stack.Screen name="Inicio" component={Inicio} options={{
+						headerShown: false
+					}} />
+					<Stack.Screen name="VerConductor" component={VerConductor} options={{
+						title: '',
+						headerStyle: {
+							backgroundColor: theme.colors.darkBlue[800]
+						},
+						headerTintColor: '#FFFFFF'
+					}} />
+					<Stack.Screen name="CrearRuta" component={CrearRuta} options={{
+						title: 'Crear una nueva ruta',
+						headerStyle: {
+							backgroundColor: theme.colors.darkBlue[800]
+						},
+						headerTintColor: '#FFFFFF'
+					}} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</NativeBaseProvider>
+	);
 }
 
 // Color Switch Component
 function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light"}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
-  );
+	const { colorMode, toggleColorMode } = useColorMode();
+	return (
+		<HStack space={2} alignItems="center">
+			<Text>Dark</Text>
+			<Switch
+				isChecked={colorMode === "light"}
+				onToggle={toggleColorMode}
+				aria-label={
+					colorMode === "light" ? "switch to dark mode" : "switch to light mode"
+				}
+			/>
+			<Text>Light</Text>
+		</HStack>
+	);
 }
